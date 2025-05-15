532d98ea10414891915c3c520f37c08ffe416b9d
dorian <dualdproductions@gmail.com>
2025-05-15 20:13:05 +0300

    test version

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const mysql = require('mysql2');
 
 const app = express();
 const port = 5000;
@@ -9,71 +8,26 @@ const port = 5000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// MySQL connection
-const connection = mysql.createConnection({
-    host: 'MacBook-Pro-6.local',
-    user: 'root',
-    password: 'Dodo755432123',
-    database: 'user_login'
-});
 
-connection.connect((err) => {
-    if (err) {
-        console.error('Error connecting to the database:', err);
-        return;
-    }
-    console.log('Connected to the MySQL database.');
-});
+
 
 // Route to handle form submission
 app.post('/submit', (req, res) => {
-    const { username, password } = req.body;
+   
 
     // Insert data into the database
-    const query = 'INSERT INTO users (username, password) VALUES (?, ?)';
-    connection.query(query, [username, password], (err, results) => {
-        if (err) {
-            console.error('Error inserting data:', err);
-            res.status(500).send('Error saving data to the database.');
-            return;
-        }
-        console.log('Data inserted:', results);
+   
         res.send('Data saved successfully into mySQL!');  
-    });
+        console.log('Data received:');
 });
 
 // Route to handle login requests
 app.get('/login_request', (req, res) => {
-    //console.log('reached the endpoint /login_request');
-    const { username, password } = req.query;
-    //console.log('identifiers', username, password);
-
-    // Retrieve data from the database
-    const query = `SELECT * FROM users WHERE username = '${username}'AND password = '${password}'`;
-
-    connection.query(query, [username, password], (err, results) => {
-        if (err) {
-            console.error('Error retrieving data:', err);
-            res.status(500).send('Error retrieving data from the database.');
-            return;
-        }
-
-        if (results.length === 0) {
-            //console.log('No matching user found.');
-            res.status(401).send('Invalid username or password.');
-            return;
-        }
-        else {
-            console.log('User found:', results);
-        
-        //console.log(results);
-        res.send(results);
+    
+        res.send("hello");
         //notify the app that the user is logged in and retrieve the corresping data from the client key
         
-        }
-
-    });
-});
+        });
 
 
 
