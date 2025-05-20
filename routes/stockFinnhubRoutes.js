7b8751a06483e9436e8e21745b111f74cfec87ba
dorian <dualdproductions@gmail.com>
2025-05-20 19:13:40 +0300

    new fix after modularity

--- routes/stockFinnhubRoutes.js
+++ routes/stockFinnhubRoutes.js
@@ -1,9 +1,8 @@
 import express from 'express';
+import { getStocks } from '../controllers/stockFinnhubController.js';
 
 const router = express.Router();
 
-router.get('/get_stocks', async (req, res) => {
-  res.json({ message: "Stocks coming soon!" });
-});
+router.get('/get_stocks', getStocks);
 
 export default router;
