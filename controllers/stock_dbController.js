5df89476b45250596dac500bf9259fa794a66acb
dorian <dualdproductions@gmail.com>
2025-05-28 16:00:40 +0300

    changed LIMIT in SQL2

--- controllers/stock_dbController.js
+++ controllers/stock_dbController.js
@@ -1,9 +1,9 @@
 import db from '../services/db.js';
 
 export const fetchallStockDB = (req, res) => {
-
+  const limit = parseInt(req.query.limit) || 10000; // default 100
   const offset = parseInt(req.query.offset) || 0;
-  db.query('SELECT * FROM stocks LIMIT 10000 OFFSET ?', [limit, offset], (err, results) => {
+  db.query('SELECT * FROM stocks LIMIT ? OFFSET ?', [limit, offset], (err, results) => {
     if (err) return res.status(500).json({ error: 'Database error' });
     res.json(results);
   });
