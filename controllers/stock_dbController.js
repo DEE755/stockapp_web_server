14576bd1c39d03bb3c9c053639d29b9aafefd1e2
dorian <dualdproductions@gmail.com>
2025-05-28 15:51:33 +0300

    testing setting with lower limit

--- controllers/stock_dbController.js
+++ controllers/stock_dbController.js
@@ -1,7 +1,7 @@
 import db from '../services/db.js';
 
 export const fetchallStockDB = (req, res) => {
-  const limit = parseInt(req.query.limit) || 100; // default 100
+  const limit = parseInt(req.query.limit) || 10; // default 100
   const offset = parseInt(req.query.offset) || 0;
   db.query('SELECT * FROM stocks LIMIT ? OFFSET ?', [limit, offset], (err, results) => {
     if (err) return res.status(500).json({ error: 'Database error' });
