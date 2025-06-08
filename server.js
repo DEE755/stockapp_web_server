3603a5453c920c6bdf4d4d70266351f54a601a67
dorian <dualdproductions@gmail.com>
2025-06-08 18:18:44 +0300

    new add2

--- server.js
+++ server.js
@@ -22,6 +22,7 @@ app.use(cors());
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Routes
 app.use('/', authRoutes);
