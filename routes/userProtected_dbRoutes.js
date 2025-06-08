7484db354b325e765447164b05d4795a71b17e5b
dorian <dualdproductions@gmail.com>
2025-06-08 16:24:42 +0300

    added functions for price updating and ma

--- routes/userProtected_dbRoutes.js
+++ routes/userProtected_dbRoutes.js
@@ -1,15 +1,28 @@
 
 import express from 'express';
 import { authenticateToken } from '../middleware/authenticateToken.js';
-import { userfollowstock } from '../controllers/stock_dbController.js';
+import { userfollowstock, getUpdateForFollowedStocksMA,getUpdateForFollowedStocksPR} from '../controllers/stock_dbController.js';
 
 const userProtectedRouter = express.Router();
 
 userProtectedRouter.use(authenticateToken);
 
-userProtectedRouter.post('/user/follows_stock', (req, res) => userfollowstock(true, req, res));
-userProtectedRouter.post('/user/unfollows_stock', (req, res) => userfollowstock(false, req, res));
+userProtectedRouter.post('/user/follows_stock', (req, res) => {
+    const userId = req.user.user_id;
+    userfollowstock(true, req, res, userId);
+});
 
+userProtectedRouter.post('/user/unfollows_stock', (req, res) => userfollowstock(false, req, res, userId));
+
+userProtectedRouter.get('/get_update_for_followed_stocksMA', (req, res) => {
+    const userId = req.user.user_id;
+    getUpdateForFollowedStocksMA(res, userId);
+});
+
+userProtectedRouter.get('/get_update_for_followed_stocksPR', (req, res) => {
+    const userId = req.user.user_id;
+    getUpdateForFollowedStocksPR(res, userId);
+});
 
 
 export default userProtectedRouter;
