3b8d3a81843dece62f03f96bc652e505afcab1de
dorian <dualdproductions@gmail.com>
2025-06-09 13:25:25 +0300

    changed API provider to avoid charges

---
+++ controllers/alphawantageController.js
@@ -0,0 +1,31 @@
+import fetch from 'node-fetch';
+
+export const getCurrentPrice = (symbol) => {
+  return new Promise((resolve, reject) => {
+    const url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=' + symbol + '&interval=1min&apikey=' + process.env.ALPHAWANTAGE_KEY;
+
+    fetch(url)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        const timeSeries = data["Time Series (1min)"];
+        if (!timeSeries) {
+          reject(new Error('No time series data found'));
+          return;
+        }
+        const latestTimestamp = Object.keys(timeSeries)[0];
+        const latestData = timeSeries[latestTimestamp];
+        const currentPrice = latestData["4. close"];
+        console.log("Current price:", currentPrice);
+        resolve(currentPrice);
+      })
+      .catch(err => {
+        console.log('Error:', err);
+        reject(err);
+      });
+  });
+};
