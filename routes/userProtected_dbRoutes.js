b1dd1e9848a660b62c61b8821d255afcb0ad5903
dorian <dualdproductions@gmail.com>
2025-06-21 23:55:21 +0300

    added functions for followset serializing

--- routes/userProtected_dbRoutes.js
+++ routes/userProtected_dbRoutes.js
@@ -1,18 +1,23 @@
 
 import express from 'express';
 import { authenticateToken } from '../middleware/authenticateToken.js';
-import { userfollowstock, getUpdateForFollowedStocksMA,getUpdateForFollowedStocksPR} from '../controllers/stock_dbController.js';
+import { userfollowstock, getUpdateForFollowedStocksMA,getUpdateForFollowedStocksPR, addNewFollowsettoDB} from '../controllers/stock_dbController.js';
 
 const userProtectedRouter = express.Router();
 
 userProtectedRouter.use(authenticateToken);
+
+//All routes here already start with /user/
 
 userProtectedRouter.post('/follows_stock', (req, res) => {
     const userId = req.user.user_id;
     userfollowstock(true, req, res, userId);
 });
 
-userProtectedRouter.post('/unfollows_stock', (req, res) => userfollowstock(false, res, req, userId));
+userProtectedRouter.post('/unfollows_stock', (req, res) => 
+    {const userId = req.user.user_id;
+        userfollowstock(false, res, req, userId);
+    });
 
 userProtectedRouter.get('/get_update_for_followed_stocksMA', (req, res) => {
     const userId = req.user.user_id;
@@ -24,5 +29,10 @@ userProtectedRouter.get('/get_update_for_followed_stocksPR', (req,res) => {
     getUpdateForFollowedStocksPR(res, userId);
 });
 
+
+userProtectedRouter.post('/followset/push', (req,res) => 
+    {addNewFollowsettoDB(req,res, req.user.user_id);
+
+    });
 
 export default userProtectedRouter;
